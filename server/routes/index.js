cddb73f4704e0e3347a5c848f5eb5b1048ebe5cf
Fahad Hayat <fahadhayat@outlook.com>
2019-02-04 12:18:59 -0500

    fix lint erros

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,12 @@
-let express = require("express");
+let express = require('express');
 let router = express.Router();
 
-router.get("/", function(req, res, next){
-	res.render("index.html")
+router.get('/', function(req, res){
+	res.render('index.html');
 }); 
 
-router.get("/api/test", function(req, res, next){
-	let data = {test: "This is test data"};
+router.get('/api/test', function(req, res){
+	let data = {test: 'This is test data'};
 	res.send(data);
 });
 
