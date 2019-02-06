9c3ad6908b9be4a2e9bc19868fd4400cc197cbc7
Fahad Hayat <fahadhayat@outlook.com>
2019-02-06 15:11:22 -0500

    Created helper functions for OCR functions

---
+++ python-server/helper.py
@@ -0,0 +1,30 @@
+import cv2
+import json
+
+# Saves an array as a JSON in a file
+
+
+def save_json(data, filename):
+    with open(filename + '.json', 'w') as outfile:
+        json.dump(data, outfile)
+
+# Returns an image read by cv2
+
+
+def read_image(filename):
+    return cv2.imread(filename)
+
+# Extract contours and hierarchy from an image
+
+
+def extract_contours(img):
+    # Gray scale the image
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    #Apply threshold to the image
+    ret, mask = cv2.threshold(gray, 220, 225, cv2.THRESH_BINARY_INV)
+
+    #IMAGE CONTOURS
+    contours, h = cv2.findContours(mask, 1, 2)
+
+    return contours, h
