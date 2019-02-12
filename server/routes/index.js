7109a0db33a34b35513b712bef657076f1df2b15
Fahad Hayat <fahadhayat@outlook.com>
2019-02-12 13:05:27 -0500

    Added a get call to the python server

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,6 @@
 let express = require('express');
 let router = express.Router();
+let request = require('request');
 const sqlite = require('sqlite3').verbose();
 
 let db = new sqlite.Database('database/Kalend.db', (err) => {
@@ -17,6 +18,15 @@ router.get('/api/test', function(req, res){
 	let data = {test: 'This is test data'};
 	res.send(data);
 });
+
+router.get('/api/analyzepicture', function(req, res){
+	request('http://localhost:5000/test', function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			console.log('This is the body:', body); // Print the google web page.
+		}
+	});
+});
+
 
 router.get('/api/users', function(req, res){
 	db.all('SELECT * FROM USER', function(err, rows) {
