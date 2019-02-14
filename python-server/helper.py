b563c8c87ad29665c96ab268714d27bd54bc7f7c
Fahad Hayat <fahadhayat@outlook.com>
2019-02-14 13:50:42 -0500

    bring back the helper func!!!!!

--- python-server/helper.py
+++ python-server/helper.py
@@ -9,3 +9,14 @@ def save_json(data, filename):
 # Returns an image read by cv2
 def read_image(filename):
     return cv2.imread(filename)
+
+# Extract contours and hierarchy from an image
+def extract_contours(img):
+    # Gray scale the image
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # Apply threshold to the image
+    ret, mask = cv2.threshold(gray, 220, 225, cv2.THRESH_BINARY_INV)
+    # Extract image countours 
+    contours, h = cv2.findContours(mask, 1, 2)
+
+    return contours, h 
