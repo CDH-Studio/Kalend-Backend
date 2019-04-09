bc391be144ad3dc06cb943c6dd42015d9c1bd26f
cuaim <fahadhayat@outlook.com>
2019-04-09 09:17:04 -0400

    Added sessions in the server side

--- server/config/middlewares.js
+++ server/config/middlewares.js
@@ -1,8 +1,25 @@
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+
+const sess = {
+	secret: 'keyboard cat',
+	cookie: {},
+	resave: false,
+    saveUninitialized: true
+}
+  
+if (app.get('env') === 'production') {
+	app.set('trust proxy', 1) // trust first proxy
+	sess.cookie.secure = true // serve secure cookies
+}
+
 
 module.exports = app => {
 	app.use(bodyParser.json({ limit: '5MB', type:'application/json'}));
 	app.use(bodyParser.urlencoded({limit: '5MB', extended: true}));
+	app.use(session(sess));
+	app.use(cookieParser())
 	app.use(morgan('dev'));
 }; 
