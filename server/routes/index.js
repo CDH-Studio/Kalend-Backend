4c4d46b185bad413545b234c7673cd27aa935da0
cuaim <fahadhayat@outlook.com>
2019-04-09 09:26:42 -0400

    Changed all functions to arrow functions

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,26 +1,14 @@
-let express = require('express');
-let router = express.Router();
-let request = require('request');
-const fs = require('fs');
-const sqlite = require('sqlite3').verbose();
+const userQueries =  require('../database/queries/userqueries');
+const schoolInfoQueries =  require('../database/queries/schoolInfoqueries');
+const eventQueries =  require('../database/queries/eventsQueries');
+const calendarCalls = require('../services/googleCalendar'); 
+const tokenGenerator = require('../services/googleTokenGeneration');
 
-let db = new sqlite.Database('database/Kalend.db', (err) => {
-	if (err) {
-		return console.error(err.message);
-	}
-	console.log('Connected to the Kalend SQLite Database');
-});
+const express = require('express');
+const router = express.Router();
+const request = require('request');
 
-router.get('/', function(req, res){
-	res.render('index.html');
-}); 
-
-router.get('/api/test', function(req, res){
-	let data = {test: 'This is test data'};
-	res.send(data);
-});
-
-router.post('/api/analyzepicture', function(req, res){
+router.post('/api/analyzepicture', (req, res) => {
 	request.post({
 		headers: {'content-type' : 'application/x-www-form-urlencoded'},
 		url:     'http://localhost:5000/analyzepicture',
@@ -31,14 +19,163 @@ router.post('/api/analyzepicture', function(req, res){
 	
 });
 
-router.get('/api/users', function(req, res){
-	db.all('SELECT * FROM USER', function(err, rows) {
-		if(err) {
+// router.get('/api/users', (req, res) => {
+// 	db.all('SELECT * FROM USER', (err, rows) => {
+// 		if(err) {
+// 			console.log(err);
+// 			return err.message;
+// 		}
+// 		res.send(JSON.stringify(rows));
+// 	});
+// });
+
+router.post('/api/logUser', async (req, res) => {
+	const { serverAuthCode, accessToken, calendarID} = req.body;
+	let {id, email, name, photo} = req.body.user;
+
+	let tokenData = await tokenGenerator.serverAuthentication(serverAuthCode);
+	let { access_token,  refresh_token }  = tokenData;
+
+	const user = {id, name, email, photo, serverAuthCode, calendarID};
+	user.accessToken = access_token;
+	user.refresToken = refresh_token;
+
+	userQueries.getUser(id)
+		.then((row) => {
+			if (row) {
+				const columns = ['FULLNAME', 'EMAIL', 'SERVERAUTHCODE', 'CALENDARID', 'ACCESSTOKEN', `REFRESHTOKEN`]
+				const values = [name, email, serverAuthCode, calendarID, access_token, refresh_token, id]
+
+				userQueries.updateUser(columns, values)
+					.then( () => {
+						req.session.userID = id;
+						res.send(true);
+					}).catch(err => {
+						res.send(false);
+					});
+			} else {
+				userQueries.insertUser(user)
+					.then(id => {
+						req.session.userID = id;
+						res.send(true);
+					})
+					.catch(err => {
+						console.log('err', err);
+						res.send(false);
+					});
+			}
+		})
+		.catch((err) => {
 			console.log(err);
+			res.send(false);
+		});
+});
+
+router.post('/api/updateUser', (req, res) => {
+	const { values, columns } = req.body;
+	const userID = req.session.userID;
+
+	userQueries.updateUser(columns, [...values, userID])
+		.then( () => {
+			res.send(true);
+		}).catch(err => {
+			res.send(false);
+		});
+});
+
+
+router.post('/api/storeSchoolInfo', (req, res) => {
+	const { startDate, endDate, value } = req.body;
+	const id = req.session.userID;
+	const schoolID = value;
+	console.log('req.sessions.school', req.session.schoolInfo)
+	let schoolInfo = {id, startDate, endDate, schoolID};
+
+	schoolInfoQueries.getSchoolInfo(id, schoolID) 
+		.then((rows) => {
+			if (rows.length > 0) {
+				schoolInfoQueries.updateSchoolInfo(schoolInfo)
+					.then(data => {
+						req.session.schoolInfo = data;
+						res.send(true);
+						return;
+					})
+					.catch(err => {
+						console.log('err', err);
+						res.send(false);
+						return err;
+					});
+			} else {
+				schoolInfoQueries.insertSchoolInfo(schoolInfo)
+					.then(data => {
+						req.session.schoolInfo = data;
+						res.send(true);
+						return;
+					})
+					.catch(err => {
+						console.log('err', err);
+						res.send(false);
+						return err;
+					});
+			}
+
+		}).catch((err) => {
+			res.send(false);
+			return err;
+		})
+});
+
+router.post('/api/storeGeneratedCalendars', async (req,res) =>  {
+	console.log('length', req.body.length);
+	let promises = [];
+	await req.body.forEach(event => {
+		promises.push(eventQueries.insertEvent(event, req.session.userID));
+	});
+
+	Promise.all(promises).then(data => {
+		console.log('data', promises);
+		res.send(true);
+	})
+	.catch(err => {
+		res.send(false);
+	});
+	
+});
+
+router.post('/api/storeUserHours', (req, res) => {
+	const { startDate, endDate, value } = req.body;
+	const id = req.session.userID;
+	console.log('id', id);
+	console.log('req.body', req.body);
+	if (req.session.info.schoolInfo != undefined) {
+		res.send(req.session.info.schoolInfo);
+		return;
+	} 
+
+	db.run('INSERT INTO UserSchoolInfo VALUES (?,?,?,?)', [id, value, startDate, endDate], (err) => {
+	 	if (err) {
+	 		console.log(err);
+			res.send(false)
 			return err.message;
 		}
-		res.send(JSON.stringify(rows));
-	});
+		req.session.info.schoolInfo = { startDate, endDate, value};
+		req.session.save();
+		res.send(true);
+
+		return;
+	 });
+
+});
+
+router.get('/api/getEvents', (req,res) =>  {
+	if (req.session.userID) {
+		eventQueries.getEvents(req.session.userID)
+			.then(events => {
+				res.send(events);
+			});
+	}
 });
 
 module.exports = router;
+
+
