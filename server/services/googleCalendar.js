497c0928db98b1bf7c5fa0dd72a03de9668c7601
cuaim <fahadhayat@outlook.com>
2019-04-09 09:13:04 -0400

    Created API calls for google calendar

---
+++ server/services/googleCalendar.js
@@ -0,0 +1,47 @@
+const request = require('request');
+
+let apiHelperCall = (URL, method, data) => {
+    console.log('data', data);
+    let fetch = request.get;
+
+	let fetchData = {
+		url: URL,
+		headers: {
+			'Accept': 'application/json',
+			'Authorization': 'Bearer ' + data.ACCESSTOKEN,
+			'Content-Type': 'application/json',
+		}
+	};
+
+	if (method == 'POST') {
+        fetchData.body = JSON.stringify(data.info);
+        fetch = request.post
+    }
+
+	return  new Promise((resolve, reject) => {
+        fetch(fetchData, (error, response, body) => {
+            if(error) reject(error);
+            resolve(JSON.parse(body));
+        });
+    });
+};
+
+
+/**
+ * Returns a list of events for the specified calendar
+ * https://developers.google.com/calendar/v3/reference/events/list
+ * 
+ * @param {String} calendarId The calendar identifier
+ * @param {Object} data The JSON object containing the optinal information for the API
+ * @param {Object} query Query parameter object to be appended to the URL
+ * 
+ * @returns {Promise} A promise containing an object with the information of every events in the specified calendar
+ */
+let listEvents = (calendarId, data, query) => {
+	return apiHelperCall('https://www.googleapis.com/calendar/v3/calendars/' + calendarId + '/events', 'GET', data, query);
+};
+
+
+module.exports = { 
+    listEvents
+};
