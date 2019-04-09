bbd95240b726035966b2b06f8c5a1ec63052f234
cuaim <fahadhayat@outlook.com>
2019-04-09 09:26:58 -0400

    modified the script file

--- server/database/script.sql
+++ server/database/script.sql
@@ -1,38 +1,60 @@
 BEGIN TRANSACTION;
 
-CREATE TABLE IF NOT EXISTS USER (
-    `ID` INT PRIMARY KEY     NOT NULL,
-    `FIRSTNAME`           TEXT    NOT NULL,
-    `LASTNAME`            TEXT     NOT NULL,
-    `ACCESSTOKEN`               TEXT     NOT NULL,
-    `IDTOKEN`               TEXT     NOT NULL,
-    `EMAIL`                 TEXT  NOT NULL,
-    `PHOTO`                 TEXT,
+DROP TABLE User;
+CREATE TABLE IF NOT EXISTS User (
+    `ID`                 TEXT PRIMARY KEY  NOT NULL,
+    `EMAIL`                 TEXT NOT NULL,
     `FULLNAME`              TEXT,
-    `SERVERAUTHCODE`        TEXT
+    `SERVERAUTHCODE`        TEXT,
+    `CALENDARID`            TEXT,
+    `FIREBASEID`            TEXT,
+    `ACCESSTOKEN`           TEXT,
+    `REFRESHTOKEN`           TEXT,
+    `PHOTOURL`              TEXT
 );
 
-INSERT INTO USER (
-   ID,
-    FIRSTNAME,
-    LASTNAME,
-    ACCESSTOKEN,
-    IDTOKEN,
-    EMAIL,
-    PHOTO,
-    FULLNAME,
-    SERVERAUTHCODE
-)
-VALUES
-    (
-        1,
-        'Fahad',
-        'Hayat',
-        'AccessToken',
-        'IDtoken',
-        'fahadhayat@outlook.com',
-        'inset url here',
-        'Fahad Hayat',
-        'Server Auth'
-    );
+CREATE TABLE IF NOT EXISTS School (
+    `ID` INTEGER PRIMARY KEY AUTOINCREMENT,
+    `NAME`                 TEXT  NOT NULL
+);
+
+
+-- INSERT INTO School (NAME) VALUES ('Carleton University');
+-- INSERT INTO School (NAME) VALUES('University of Ottawa');
+-- DROP TABLE UserSchoolInfo;
+CREATE TABLE IF NOT EXISTS UserSchoolInfo (
+    `USERID`            TEXT   NOT NULL,
+    `SCHOOLID`            INTEGER NOT NULL,
+    `START`                 TEXT NOT NULL,
+    `END`                   TEXT NOT NULL,
+    FOREIGN KEY(`USERID`) REFERENCES User(`ID`),
+    FOREIGN KEY (`SCHOOLID`) REFERENCES School(`ID`),
+    PRIMARY KEY (`USERID`, `SCHOOLID`)
+);
+
+CREATE TABLE IF NOT EXISTS EventType (
+    `ID` INTEGER PRIMARY KEY AUTOINCREMENT,
+    `CATEGORY`                 INTEGER  NOT NULL
+);
+
+-- INSERT INTO EventType (CATEGORY) VALUES ('AI');
+-- INSERT INTO EventType (CATEGORY) VALUES('FIXED');
+-- INSERT INTO EventType (CATEGORY) VALUES('SCHOOL');
+DROP TABLE UserEvent;
+CREATE TABLE IF NOT EXISTS  UserEvent (
+    `ID`                    TEXT PRIMARY KEY  NOT NULL,
+    `USERID`                TEXT NOT NULL,
+    `CATEGORY`               TEXT,
+    `START`                 DATE NOT NULL,
+    `END`                   DATE NOT NULL,
+    `SUMMARY`                 TEXT NOT NULL,
+    `RECURRENCE`                TEXT,
+    `LOCATION`                   TEXT,
+    `DESCRIPTION`                   TEXT,
+    `CREATED`                       datetime NOT NULL  DEFAULT current_timestamp,
+    `UPDATED`                       datetime NOT NULL  DEFAULT current_timestamp,
+    FOREIGN KEY(USERID) REFERENCES User(ID),
+    FOREIGN KEY(CATEGORY) REFERENCES EventType(ID)
+);
+
 END TRANSACTION;
