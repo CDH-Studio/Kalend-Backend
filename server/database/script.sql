d61ad229cc190911246a55e31d4e2d2253066044
cuaim <fahadhayat@outlook.com>
2019-04-10 15:10:55 -0400

    modified script tag to include updated triggers

--- server/database/script.sql
+++ server/database/script.sql
@@ -1,6 +1,6 @@
 BEGIN TRANSACTION;
 
-DROP TABLE User;
+-- DROP TABLE User;
 CREATE TABLE IF NOT EXISTS User (
     `ID`                 TEXT PRIMARY KEY  NOT NULL,
     `EMAIL`                 TEXT NOT NULL,
@@ -10,8 +10,15 @@ CREATE TABLE IF NOT EXISTS User (
     `FIREBASEID`            TEXT,
     `ACCESSTOKEN`           TEXT,
     `REFRESHTOKEN`           TEXT,
-    `PHOTOURL`              TEXT
+    `PHOTOURL`              TEXT,
+    `CREATED`                       datetime NOT NULL  DEFAULT current_timestamp,
+    `UPDATED`                       datetime NOT NULL  DEFAULT current_timestamp
 );
+
+CREATE TRIGGER IF NOT EXISTS userTrigger AFTER UPDATE ON User
+    BEGIN
+        update User SET UPDATED = datetime('now') WHERE ID = NEW.ID;
+    END;
 
 DROP TABLE School;
 CREATE TABLE IF NOT EXISTS School (
@@ -29,10 +36,17 @@ CREATE TABLE IF NOT EXISTS UserSchoolInfo (
     `SCHOOLID`            INTEGER NOT NULL,
     `START`                 TEXT NOT NULL,
     `END`                   TEXT NOT NULL,
+    `CREATED`                       datetime NOT NULL  DEFAULT current_timestamp,
+    `UPDATED`                       datetime NOT NULL  DEFAULT current_timestamp,
     FOREIGN KEY(`USERID`) REFERENCES User(`ID`),
     FOREIGN KEY (`SCHOOLID`) REFERENCES School(`ID`),
     PRIMARY KEY (`USERID`, `SCHOOLID`)
 );
+
+CREATE TRIGGER IF NOT EXISTS userSchoolInfoUpdateTrigger AFTER UPDATE ON UserSchoolInfo
+    BEGIN
+        update UserSchoolInfo SET UPDATED = datetime('now') WHERE USERID = NEW.USERID AND SCHOOLID = NEW.SCHOOLID ;
+    END;
 
 DROP TABLE EventType;
 CREATE TABLE IF NOT EXISTS EventType (
@@ -63,6 +77,11 @@ CREATE TABLE IF NOT EXISTS  UserEvent (
     FOREIGN KEY(CATEGORY) REFERENCES EventType(ID)
 );
 
+CREATE TRIGGER IF NOT EXISTS userEventsUpdateTrigger AFTER UPDATE ON UserEvent
+    BEGIN
+        update UserEvent SET UPDATED = datetime('now') WHERE ID = NEW.ID;
+    END;
+
 DROP TABLE UnavailableHours;
 CREATE TABLE IF NOT EXISTS  UnavailableHours (
     `ID` INTEGER  auto_increment,
@@ -76,6 +95,10 @@ CREATE TABLE IF NOT EXISTS  UnavailableHours (
     FOREIGN KEY(USERID) REFERENCES User(ID),
     PRIMARY KEY (`WEEK`, `CATEGORY`, `USERID`)
 );
+CREATE TRIGGER IF NOT EXISTS hoursUpdateTrigger AFTER UPDATE ON UnavailableHours
+    BEGIN
+        update UnavailableHours SET UPDATED = datetime('now') WHERE USERID = NEW.USERID;
+    END;
 
 
 END TRANSACTION;
